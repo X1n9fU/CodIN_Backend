1e8422d20c2284a2738d5fa9b24bfc94d2e376f7
kbm <rovin1273@gmail.com>
2025-07-03 17:17:30 +0900

    refactor: Block 도메인 미사용 Exception 제거

--- src/main/java/inu/codin/codin/domain/block/exception/NotBlockedException.java
+++
@@ -1,7 +0,0 @@
-package inu.codin.codin.domain.block.exception;
-
-public class NotBlockedException extends RuntimeException{
-    public NotBlockedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/inu/codin/codin/domain/block/exception/SelfBlockedException.java
+++
@@ -1,7 +0,0 @@
-package inu.codin.codin.domain.block.exception;
-
-public class SelfBlockedException extends RuntimeException{
-    public SelfBlockedException(String message){
-        super(message);
-    }
-}
