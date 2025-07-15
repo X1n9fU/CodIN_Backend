195cf9656ab20074c7cfe9f4fb8935c3c7bb1008
X1n9fU <minkyeong0244@gmail.com>
2025-07-15 22:02:31 +0900

    Revert "fix : google oauth2 로그인 시 Url Param을 통해 프론트 측 화면으로 redirect 되는 로직 추가"
    This reverts commit a470fc13106c6182aa5568f9801a79d20a2710fe.

--- src/main/java/inu/codin/codin/common/security/controller/AuthController.java
+++ src/main/java/inu/codin/codin/common/security/controller/AuthController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -33,7 +34,7 @@ public class AuthController {
     public ResponseEntity<SingleResponse<?>> googleLogin(HttpServletResponse response,
                                                          @RequestParam(required = false, value = "redirect_url") String redirect_url) throws IOException {
         authSessionService.setSession(redirect_url);
-        response.sendRedirect("/api/oauth2/authorization/google");
+        response.sendRedirect("/oauth2/authorization/google");
         return ResponseEntity.ok()
                 .body(new SingleResponse<>(200, "google OAuth2 Login Redirect",null));
     }
